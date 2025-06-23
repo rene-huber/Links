56325d1502201154e550d97dd14d09d4c63af2ce
rene-huber <96597641+rene-huber@users.noreply.github.com>
2025-06-23 23:34:03 +0200

    margin on text

--- script.js
+++ script.js
@@ -175,10 +175,8 @@ function openPortfolioModal(project) {
                 <h3>${project.title}</h3>
                 <p>${project.description}</p>
                 <a href="${project.url}" target="_blank" class="visit-project-btn">
-                    <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"/>
-                    </svg>
-                    Visitar Proyecto
+               
+                    View Project
                 </a>
             </div>
         </div>
